5b209a1f79dea6ff2e0729756b255010f9c65a0e
Marios Kogias <marioskogias@grnet.gr>
2014-05-08 13:05:48 +0300

    Add a C binding for facebook-scribe python module
    This is only a test file. This will end up to a library to inlude
    in your source files.

---
+++ scribe.c
@@ -0,0 +1,159 @@
+#include <Python.h>
+
+int main() {
+
+    PyObject *scribe_name, * transport_name, *socket_name, *protocol_name;
+    PyObject *scribe_module, * transport_module, *socket_module, *protocol_module;
+    PyObject * pArgs, *pValue, *pFunc;
+    PyObject * socket, *protocol, *transport, *client;
+
+    Py_Initialize();
+    scribe_name = PyString_FromString("scribe.scribe");
+    socket_name = PyString_FromString("thrift.transport.TSocket");
+    transport_name = PyString_FromString("thrift.transport.TTransport");
+    protocol_name = PyString_FromString("thrift.protocol.TBinaryProtocol");
+    
+    
+    scribe_module = PyImport_Import(scribe_name);
+    Py_DECREF(scribe_name);
+
+    transport_module = PyImport_Import(transport_name);
+    Py_DECREF(transport_name);
+    
+    socket_module = PyImport_Import(socket_name);
+    Py_DECREF(socket_name);
+    
+    protocol_module = PyImport_Import(protocol_name);
+    Py_DECREF(protocol_name);
+
+    //Initialize the socket
+    if (socket_module != NULL) {
+
+        pFunc = PyObject_GetAttrString(socket_module, "TSocket");
+        if (pFunc && PyCallable_Check(pFunc)) {
+            pArgs = PyTuple_New(2);
+
+            //set host
+            pValue = PyString_FromString("83.212.113.88");
+            PyTuple_SetItem(pArgs, 0, pValue);
+
+            //set port
+            pValue = PyInt_FromLong(1463);
+            PyTuple_SetItem(pArgs, 1, pValue);
+
+            socket = PyObject_CallObject(pFunc, pArgs);
+            if (!socket)
+                printf("error socket\n");
+        } else 
+            printf("error no socket function\n");
+    } else
+        printf("error no socket module\n");
+
+    //initialize transport
+    if (transport_module != NULL) {
+
+        pFunc = PyObject_GetAttrString(socket_module, "TFramedTransport");
+        if (pFunc && PyCallable_Check(pFunc)) {
+            pArgs = PyTuple_New(1);
+
+            //set socket
+            PyTuple_SetItem(pArgs, 0, socket);
+
+            transport = PyObject_CallObject(pFunc, pArgs);
+            if (!transport)
+                printf("error transport");
+        } else 
+            printf("error no transport function\n");
+    } else
+        printf("error no transport module\n");
+
+    //initialize protocol
+    if (protocol_module != NULL) {
+
+        pFunc = PyObject_GetAttrString(protocol_module, "TBinaryProtocol");
+        if (pFunc && PyCallable_Check(pFunc)) {
+            pArgs = PyTuple_New(3);
+
+            //set transport
+            PyTuple_SetItem(pArgs, 0, transport);
+
+            //set booleans  
+            pValue = PyBool_FromLong(0);
+            PyTuple_SetItem(pArgs, 1, pValue);
+            PyTuple_SetItem(pArgs, 2, pValue);
+
+            protocol = PyObject_CallObject(pFunc, pArgs);
+            if (!protocol)
+                printf("error protocol");
+        } else 
+            printf("error no protocol function\n");
+    } else
+        printf("error no protocol module\n");
+    
+    //initialize client
+    if (scribe_module != NULL) {
+        
+        pFunc = PyObject_GetAttrString(scribe_module, "Client");
+        if (pFunc && PyCallable_Check(pFunc)) {
+        pArgs = PyTuple_New(1);
+
+        //set protocol
+        PyTuple_SetItem(pArgs, 0, protocol);
+       
+        client = PyObject_CallObject(pFunc, pArgs);
+        if (!client)
+            printf("error client");
+    } else 
+        printf("error no client function \n");
+    } else
+        printf("error no client module\n");
+
+    //open transport
+    pFunc = PyObject_GetAttrString(transport, "open");
+    if (pFunc && PyCallable_Check(pFunc)) {
+        pArgs = PyTuple_New(0);
+
+        pValue = PyObject_CallObject(pFunc, pArgs);
+    } else 
+        printf("error no open function \n");
+
+    
+    //create log entry
+    PyObject * category, *message;
+    category = PyString_FromString("LOGS");
+    message = PyString_FromString("Hello from C");
+
+   //create log entry 
+    pFunc = PyObject_GetAttrString(scribe_module, "LogEntry");
+    if (pFunc && PyCallable_Check(pFunc)) {
+        pArgs = PyTuple_New(2);
+
+        //set category
+        PyTuple_SetItem(pArgs, 0, category);
+
+        //set message
+        PyTuple_SetItem(pArgs, 1, message);
+
+        pValue = PyObject_CallObject(pFunc, pArgs);
+        if (!pValue)
+            printf("error no log value");
+    } else 
+        printf("error no log entry function\n");
+
+    PyObject* messages = PyList_New(1);
+    PyList_SetItem(messages, 0, pValue);
+    
+    //get the log  function
+    pFunc = PyObject_GetAttrString(client, "Log");
+    if (pFunc && PyCallable_Check(pFunc)) {
+        pArgs = PyTuple_New(1);
+
+        //set message
+        PyTuple_SetItem(pArgs, 0, messages);
+
+        pValue = PyObject_CallObject(pFunc, pArgs);
+        if (!pValue)
+            printf("error no result value");
+    } else 
+        printf("error no log entry function\n");
+}
